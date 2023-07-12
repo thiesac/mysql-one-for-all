ccd4aada51c441c94d5b9ce2fb801e17c301e36c
Thiesa <thiesa.c@gmail.com>
2023-07-12 08:34:31 -0400

    Requisito 1 passando

--- challenges/desafio1.sql
+++ challenges/desafio1.sql
@@ -58,80 +58,92 @@ CREATE TABLE historico_reproducoes(
 );
 
 INSERT INTO planos (plano, valor_plano) 
-VALUES ('gratuito', 0);
-('familiar', 7.99);
-('universitário', 5.99);
+VALUES 
+('gratuito', 0),
+('familiar', 7.99),
+('universitário', 5.99),
 ('pessoal', 6.99);
 
-INSERT INTO pessoa_usuaria (nome_pessoa_usuaria, idade, plano_id, data_assinatura) VALUES ('Barbara Liskov', 82, 1, '2019-10-20');
-INSERT INTO pessoa_usuaria (nome_pessoa_usuaria, idade, plano_id, data_assinatura) VALUES ('Robert Cecil Martin', 58, 1, '2017-01-06');
-INSERT INTO pessoa_usuaria (nome_pessoa_usuaria, idade, plano_id, data_assinatura) VALUES ('Ada Lovelace', 37, 2, '2017-12-30');
-INSERT INTO pessoa_usuaria (nome_pessoa_usuaria, idade, plano_id, data_assinatura) VALUES ('Martin Fowler', 46, 2, '2017-01-17');
-INSERT INTO pessoa_usuaria (nome_pessoa_usuaria, idade, plano_id, data_assinatura) VALUES ('Sandi Metz', 58, 2, '2018-04-29');
-INSERT INTO pessoa_usuaria (nome_pessoa_usuaria, idade, plano_id, data_assinatura) VALUES ('Paulo Freire', 19, 3, '2018-02-14');
-INSERT INTO pessoa_usuaria (nome_pessoa_usuaria, idade, plano_id, data_assinatura) VALUES ('Bell Hooks', 26, 3, '2018-01-05');
-INSERT INTO pessoa_usuaria (nome_pessoa_usuaria, idade, plano_id, data_assinatura) VALUES ('Christopher Alexander', 85, 4, '2019-06-05');
-INSERT INTO pessoa_usuaria (nome_pessoa_usuaria, idade, plano_id, data_assinatura) VALUES ('Judith Butler', 45, 4, '2020-05-13');
-INSERT INTO pessoa_usuaria (nome_pessoa_usuaria, idade, plano_id, data_assinatura) VALUES ('Jorge Amado', 58, 4, '2017-02-17');
+INSERT INTO artistas (artista)
+VALUES
+    ('Beyoncé'),
+    ('Queen'),
+    ('Elis Regina'),
+    ('Baco Exu do Blues'),
+    ('Blind Guardian'),
+    ('Nina Simone');
 
-INSERT INTO artistas (artista) VALUES ('Beyoncé');
-INSERT INTO artistas (artista) VALUES ('Queen');
-INSERT INTO artistas (artista) VALUES ('Queen');
-INSERT INTO artistas (artista) VALUES ('Elis Regina');
-INSERT INTO artistas (artista) VALUES ('Elis Regina');
-INSERT INTO artistas (artista) VALUES ('Baco Exu do Blues');
-INSERT INTO artistas (artista) VALUES ('Blind Guardian');
-INSERT INTO artistas (artista) VALUES ('Nina Simone');
+INSERT INTO albuns (album, artista_id, ano_lancamento) 
+VALUES 
+('Renaissance', 1, 2022),
+('Jazz', 2, 1978),
+('Hot Space', 2, 1982),
+('Falso Brilhante', 3, 1998),
+('Vento de Maio', 3, 2001),
+('QVVJFA?', 4, 2003),
+('Somewhere Far Beyond', 5, 2007),
+('I Put A Spell On You', 6, 2012);
 
-INSERT INTO albuns (album, artista_id, ano_lancamento) VALUES ('Renaissance', '1', '2022');
-INSERT INTO albuns (album, artista_id, ano_lancamento) VALUES ('Jazz', '2', '1978');
-INSERT INTO albuns (album, artista_id, ano_lancamento) VALUES ('Hot Space', '3', '1982');
-INSERT INTO albuns (album, artista_id, ano_lancamento) VALUES ('Falso Brilhante', '4', '1998');
-INSERT INTO albuns (album, artista_id, ano_lancamento) VALUES ('Vento de Maio', '5', '2001');
-INSERT INTO albuns (album, artista_id, ano_lancamento) VALUES ('QVVJFA?', '6', '2003');
-INSERT INTO albuns (album, artista_id, ano_lancamento) VALUES ('Somewhere Far Beyond', '7', '2007');
-INSERT INTO albuns (album, artista_id, ano_lancamento) VALUES ('I Put A Spell On You', '8', '2012');
+INSERT INTO cancoes (cancoes, duracao_segundos, album_id)
+ VALUES 
+('BREAK MY SOUL', 279, 1),
+('VIRGOS GROOVE', 369, 1),
+('ALIEN SUPERSTAR', 116, 1),
+('Dont Stop Me Now', 203, 2),
+('Under Pressure', 152, 2),
+('Como Nossos Pais', 105, 4),
+('O Medo de Amar é o Medo de Ser Livre', 207, 5),
+('Samba em Paris', 267, 6),
+('The Bards Song', 244, 7),
+('Feeling Good"', 100, 8);
 
-INSERT INTO cancoes (cancoes, duracao_segundos, album_id) VALUES ('BREAK MY SOUL', '279', '1');
-INSERT INTO cancoes (cancoes, duracao_segundos, album_id) VALUES ('VIRGOS GROOVE', '369', '1');
-INSERT INTO cancoes (cancoes, duracao_segundos, album_id) VALUES ('ALIEN SUPERSTAR', '116', '1');
-INSERT INTO cancoes (cancoes, duracao_segundos, album_id) VALUES ('Dont Stop Me Now', '203', '2');
-INSERT INTO cancoes (cancoes, duracao_segundos, album_id) VALUES ('Under Pressure', '152', '2');
-INSERT INTO cancoes (cancoes, duracao_segundos, album_id) VALUES ('Como Nossos Pais', '105', '4');
-INSERT INTO cancoes (cancoes, duracao_segundos, album_id) VALUES ('O Medo de Amar é o Medo de Ser Livre', '207', '5');
-INSERT INTO cancoes (cancoes, duracao_segundos, album_id) VALUES ('Samba em Paris', '267', '6');
-INSERT INTO cancoes (cancoes, duracao_segundos, album_id) VALUES ('The Bards Song', '244', '7');
-INSERT INTO cancoes (cancoes, duracao_segundos, album_id) VALUES ('Feeling Good', '100', '8');
+INSERT INTO pessoa_usuaria (nome_pessoa_usuaria, idade, plano_id, data_assinatura) 
+VALUES 
+('Barbara Liskov', 82, 1, '2019-10-20'),
+('Robert Cecil Martin', 58, 1, '2017-01-06'),
+('Ada Lovelace', 37, 2, '2017-12-30'),
+('Martin Fowler', 46, 2, '2017-01-17'),
+('Sandi Metz', 58, 2, '2018-04-29'),
+('Paulo Freire', 19, 3, '2018-02-14'),
+('Bell Hooks', 26, 3, '2018-01-05'),
+('Christopher Alexander', 85, 4, '2019-06-05'),
+('Judith Butler', 45, 4, '2020-05-13'),
+('Jorge Amado', 58, 4, '2017-02-17');
 
-INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) VALUES (8, '2022-02-28 10:45:55', 1);
-INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) VALUES (2, '2020-05-02 05:30:35', 1);
-INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) VALUES (10, '2020-03-06 11:22:33', 1);
-INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) VALUES (10, '2022-08-05 08:05:17', 2);
-INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) VALUES (7, '2020-01-02 07:40:33', 2);
-INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) VALUES (10, '2020-11-13 16:55:13', 3);
-INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) VALUES (2, '2020-12-05 18:38:30', 3);
-INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) VALUES (8, '2021-08-15 17:10:10', 4);
-INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) VALUES (8, '2022-01-09 01:44:33', 5);
-INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) VALUES (5, '2020-08-06 15:23:43', 5);
-INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) VALUES (7, '2017-01-24 00:31:17', 6);
-INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) VALUES (1, '2017-10-12 12:35:20', 6);
-INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) VALUES (4, '2011-12-15 22:30:49', 7);
-INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) VALUES (4, '2012-03-17 14:56:41', 8);
-INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) VALUES (9, '2022-02-24 21:14:22', 9);
-INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) VALUES (3, '2015-12-13 08:30:22', 10);
+INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) 
+VALUES 
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 1),
+(2, 3),
+(3, 2),
+(4, 4),
+(5, 5),
+(5, 6),
+(6, 6),
+(6, 1),
+(7, 6),
+(9, 3),
+(10, 2);
 
-INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) VALUES (1, 1);
-INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) VALUES (1, 2);
-INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) VALUES (1, 4);
-INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) VALUES (2, 1);
-INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) VALUES (2, 5);
-INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) VALUES (3, 2);
-INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) VALUES (4, 6);
-INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) VALUES (5, 7);
-INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) VALUES (5, 8);
-INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) VALUES (6, 1);
-INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) VALUES (6, 1);
-INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) VALUES (7, 8);
-INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) VALUES (8, 3);
-INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) VALUES (9, 5);
-INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) VALUES (10, 3);
+INSERT INTO historico_reproducoes (cancao_id, data_reproducao, pessoa_usuaria_id) 
+VALUES 
+(8, '2022-02-28 10:45:55', 1),
+(2, '2020-05-02 05:30:35', 1),
+(10, '2020-03-06 11:22:33', 1),
+(10, '2022-08-05 08:05:17', 2),
+(7, '2020-01-02 07:40:33', 2),
+(10, '2020-11-13 16:55:13', 3),
+(2, '2020-12-05 18:38:30', 3),
+(8, '2021-08-15 17:10:10', 4),
+(8, '2022-01-09 01:44:33', 5),
+(5, '2020-08-06 15:23:43', 5),
+(7, '2017-01-24 00:31:17', 6),
+(1, '2017-10-12 12:35:20', 6),
+(4, '2011-12-15 22:30:49', 7),
+(4, '2012-03-17 14:56:41', 8),
+(9, '2022-02-24 21:14:22', 9),
+(3, '2015-12-13 08:30:22', 10);
+
+
