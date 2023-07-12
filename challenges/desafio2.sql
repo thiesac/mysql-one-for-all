c00655e872e713dca2b260bc407438adc72e84ea
Thiesa <thiesa.c@gmail.com>
2023-07-12 11:58:04 -0400

    Requisito 2 funcionando

--- challenges/desafio2.sql
+++ challenges/desafio2.sql
@@ -1,9 +1,8 @@
-SELECT 
+   SELECT 
     COUNT(DISTINCT c.cancao_id) AS 'cancoes',
     COUNT(DISTINCT ar.artista_id) AS 'artistas',
     COUNT(DISTINCT al.album_id) AS 'albuns'
 FROM 
     cancoes c
-    INNER JOIN albuns al ON c.album_id = al.album_id
+    RIGHT JOIN albuns al ON c.album_id = al.album_id
     INNER JOIN artistas ar ON al.artista_id = ar.artista_id;
-
