d7bc0eac42197a579d21210c02dc3165998163b5
Thiesa <thiesa.c@gmail.com>
2023-07-12 11:20:17 -0400

    Requisito 2 com problema

--- challenges/desafio1.sql
+++ challenges/desafio1.sql
@@ -57,58 +57,58 @@ CREATE TABLE historico_reproducoes(
     FOREIGN KEY (cancao_id) REFERENCES cancoes(cancao_id)
 );
 
-INSERT INTO planos (plano, valor_plano) 
+INSERT INTO planos (plano_id, plano, valor_plano) 
 VALUES 
-('gratuito', 0),
-('familiar', 7.99),
-('universitário', 5.99),
-('pessoal', 6.99);
+(1, 'gratuito', 0),
+(2, 'familiar', 7.99),
+(3, 'universitário', 5.99),
+(4, 'pessoal', 6.99);
 
-INSERT INTO artistas (artista)
+INSERT INTO artistas (artista_id, artista)
 VALUES
-    ('Beyoncé'),
-    ('Queen'),
-    ('Elis Regina'),
-    ('Baco Exu do Blues'),
-    ('Blind Guardian'),
-    ('Nina Simone');
+    (1, 'Beyoncé'),
+    (2, 'Queen'),
+    (3, 'Elis Regina'),
+    (4, 'Baco Exu do Blues'),
+    (5, 'Blind Guardian'),
+    (6, 'Nina Simone');
 
-INSERT INTO albuns (album, artista_id, ano_lancamento) 
+INSERT INTO albuns (album_id, album, artista_id, ano_lancamento) 
 VALUES 
-('Renaissance', 1, 2022),
-('Jazz', 2, 1978),
-('Hot Space', 2, 1982),
-('Falso Brilhante', 3, 1998),
-('Vento de Maio', 3, 2001),
-('QVVJFA?', 4, 2003),
-('Somewhere Far Beyond', 5, 2007),
-('I Put A Spell On You', 6, 2012);
+(1, 'Renaissance', 1, 2022),
+(2, 'Jazz', 2, 1978),
+(3, 'Hot Space', 2, 1982),
+(4, 'Falso Brilhante', 3, 1998),
+(5, 'Vento de Maio', 3, 2001),
+(6, 'QVVJFA?', 4, 2003),
+(7, 'Somewhere Far Beyond', 5, 2007),
+(8, 'I Put A Spell On You', 6, 2012);
 
-INSERT INTO cancoes (cancoes, duracao_segundos, album_id)
+INSERT INTO cancoes (cancao_id, cancoes, duracao_segundos, album_id)
  VALUES 
-('BREAK MY SOUL', 279, 1),
-('VIRGOS GROOVE', 369, 1),
-('ALIEN SUPERSTAR', 116, 1),
-('Dont Stop Me Now', 203, 2),
-('Under Pressure', 152, 2),
-('Como Nossos Pais', 105, 4),
-('O Medo de Amar é o Medo de Ser Livre', 207, 5),
-('Samba em Paris', 267, 6),
-('The Bards Song', 244, 7),
-('Feeling Good"', 100, 8);
+(1, 'BREAK MY SOUL', 279, 1),
+(2, 'VIRGOS GROOVE', 369, 1),
+(3, 'ALIEN SUPERSTAR', 116, 1),
+(4, 'Dont Stop Me Now', 203, 2),
+(5, 'Under Pressure', 152, 2),
+(6, 'Como Nossos Pais', 105, 4),
+(7, 'O Medo de Amar é o Medo de Ser Livre', 207, 5),
+(8, 'Samba em Paris', 267, 6),
+(9, 'The Bards Song', 244, 7),
+(10, 'Feeling Good"', 100, 8);
 
-INSERT INTO pessoa_usuaria (nome_pessoa_usuaria, idade, plano_id, data_assinatura) 
+INSERT INTO pessoa_usuaria (pessoa_usuaria_id, nome_pessoa_usuaria, idade, plano_id, data_assinatura) 
 VALUES 
-('Barbara Liskov', 82, 1, '2019-10-20'),
-('Robert Cecil Martin', 58, 1, '2017-01-06'),
-('Ada Lovelace', 37, 2, '2017-12-30'),
-('Martin Fowler', 46, 2, '2017-01-17'),
-('Sandi Metz', 58, 2, '2018-04-29'),
-('Paulo Freire', 19, 3, '2018-02-14'),
-('Bell Hooks', 26, 3, '2018-01-05'),
-('Christopher Alexander', 85, 4, '2019-06-05'),
-('Judith Butler', 45, 4, '2020-05-13'),
-('Jorge Amado', 58, 4, '2017-02-17');
+(1, 'Barbara Liskov', 82, 1, '2019-10-20'),
+(2, 'Robert Cecil Martin', 58, 1, '2017-01-06'),
+(3, 'Ada Lovelace', 37, 2, '2017-12-30'),
+(4, 'Martin Fowler', 46, 2, '2017-01-17'),
+(5, 'Sandi Metz', 58, 2, '2018-04-29'),
+(6, 'Paulo Freire', 19, 3, '2018-02-14'),
+(7, 'Bell Hooks', 26, 3, '2018-01-05'),
+(8, 'Christopher Alexander', 85, 4, '2019-06-05'),
+(9, 'Judith Butler', 45, 4, '2020-05-13'),
+(10, 'Jorge Amado', 58, 4, '2017-02-17');
 
 INSERT INTO seguindo_artistas (pessoa_usuaria_id, artista_id) 
 VALUES 
---
+++ challenges/desafio2.sql
@@ -0,0 +1,9 @@
+SELECT 
+    COUNT(DISTINCT c.cancao_id) AS 'cancoes',
+    COUNT(DISTINCT ar.artista_id) AS 'artistas',
+    COUNT(DISTINCT al.album_id) AS 'albuns'
+FROM 
+    cancoes c
+    INNER JOIN albuns al ON c.album_id = al.album_id
+    INNER JOIN artistas ar ON al.artista_id = ar.artista_id;
+
